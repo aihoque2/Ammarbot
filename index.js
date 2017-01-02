f35855d01d5e8526193539a7e15be794c50ff576
Ammar Hoque <aihoque2@illinois.edu>
2017-01-02 18:45:12 -0500

    now functional, i hope

--- index.js
+++ index.js
@@ -16,12 +16,46 @@ app.get('/',function(req,res){
 	res.send("Hi, I'm Ammarbot!")
 })
 
+token="EAADa6pm6wIcBAIuau43CLuEMZAhMwVPaRPOOu3VZCdiPKbco5Q1F5SvJpRuijcXU9Cz2MsiVpcuHmk0sOZBZC3dlzpOMSksDNZCs9nihllzlxEe7FPzwsZCPzIlgMkdEgsms6diplZA4BQc2RnOOPanHJZCZBqa4Me7egs0ZAHaOprZBwZDZD"
+
 app.get('/webhook/', function(req, res){
 	if (req.query['hub.verify_token']==="ammarbot"){
 		res.send(req.query['hub.challenge'])
 	}
 	res.send("Wrong token")
 })
+
+app.post('/webhook/',function(req,res){
+	let messaging_events= req.body.entry[0].messaging_events
+	for (let i=0;i<messaging_events.length;i++){
+		let event=messaging_events[i]
+		let sender=event.sender.id
+		if(event.message && event.message.text){
+			let text = event.message.text
+			sendText(sender, "Text echo: " + text.substring(0,100))
+		}
+	}
+	res.sendStatus(200)
+})
+
+function sendText(sender, text){
+	let messageData={text: text}
+	request{
+		url: "https://graph.facebook.com/v2.6/me/messages",
+		qs:{access_token, token}
+		method: "POST"
+		json{
+			receipt: {id: sender},
+			message:messageData
+		}
+	}, function(error, response, body){
+		if (error){
+			console.log("sending error")
+		} else if (response.body.error){
+			console.log("response body error")
+		}
+	}
+}
 
 app.listen(app.get('port'), function(){
 	console.log("running: port")
